9aa6b637170da796b434bc8e6de7f8cde0b102b0
Luc'HACK <espiet.l@valbion.com>
2021-11-12 18:50:35 +0100

    ❌ Suppression d'un friend

--- resources/views/eleve/modules/nav-friends.blade.php
+++ resources/views/eleve/modules/nav-friends.blade.php
@@ -6,12 +6,12 @@
       </button>
   </div>
 
-  {{-- <ul>
+  <ul>
     <li class="relative px-6 py-3">
       <div
         class="inline-flex items-center justify-between w-full font-semibold">
         <span class="inline-flex items-center transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-          <a class="flex items-center" href="/eleve/amis/1">
+          <a class="flex items-center" href="">
             <!-- Avatar with inset shadow -->
             <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
               <img alt="" class="object-cover w-full h-full rounded-full" loading="lazy"
@@ -21,7 +21,7 @@
             <div>
               <p class="font-semibold text-sm">Lucas ESPIET</p>
               <p class="text-xs text-gray-400 dark:text-gray-600">
-                SECONDE 3
+                Seconde 3
               </p>
             </div>
           </a>
@@ -35,32 +35,3 @@
         </button>
       </div>
     </li>
-    <li class="relative px-6 py-3">
-      <div
-        class="inline-flex items-center justify-between w-full font-semibold">
-        <span class="inline-flex items-center transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-          <a class="flex items-center" href="/eleve/amis/2">
-            <!-- Avatar with inset shadow -->
-            <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-              <img alt="" class="object-cover w-full h-full rounded-full" loading="lazy"
-                src="https://i.pinimg.com/280x280_RS/ae/3e/c5/ae3ec53d9634b6ae98f2a1a7a7e306d9.jpg">
-              <div aria-hidden="true" class="absolute inset-0 rounded-full shadow-inner"></div>
-            </div>
-            <div>
-              <p class="font-semibold text-sm">Salomé CARPE</p>
-              <p class="text-xs text-gray-400 dark:text-gray-600">
-                SECONDE 3
-              </p>
-            </div>
-          </a>
-        </span>
-        <button class="transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-          <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"></path>
-          </svg>
-        </button>
-      </div>
-    </li>
-  </ul> --}}
